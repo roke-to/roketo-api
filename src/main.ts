6ec6d7654bf9a911d9560752153bbad53753dc8f
Serge K Lebedev <hello@sergelebe.dev>
2022-03-28 15:10:10 +0600

    Enable unconfigured CORS

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // TODO: Set up CORS properly for allowed origins
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
