054b0da52a01afc68c0b55aba0d3eaac7aba43c3
Serge K Lebedev <hello@sergelebe.dev>
2022-04-05 15:37:43 +0600

    Implement API client regeneration script

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import {
   SwaggerDocumentOptions,
   SwaggerModule,
 } from '@nestjs/swagger';
+import { resolve } from 'path';
+import { writeFileSync } from 'fs';
 
 import { AppModule } from './app.module';
 
@@ -14,13 +16,24 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Roke.to')
     .setDescription('The best realtime streaming solution')
-    .setVersion('1.0')
+    .setVersion('0.0.1')
+    .addServer('https://roketo-test-api.herokuapp.com')
     .addBearerAuth()
     .build();
   const options: SwaggerDocumentOptions = {
     operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
   };
   const document = SwaggerModule.createDocument(app, config, options);
+
+  if (process.env.GENERATE_SWAGGER_JSON) {
+    const outputPath = resolve(process.cwd(), 'swagger.json');
+    writeFileSync(outputPath, JSON.stringify(document), { encoding: 'utf8' });
+
+    await app.close();
+
+    return;
+  }
+
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000);
