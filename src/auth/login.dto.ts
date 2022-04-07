a665aaddc7e3e6296371f7338e25753039e12668
Serge K Lebedev <hello@sergelebe.dev>
2022-04-07 20:27:16 +0600

    ROKETO-2 Simplify auth logic

--- src/auth/login.dto.ts
+++ src/auth/login.dto.ts
@@ -5,52 +5,23 @@ import {
   Max,
   Min,
   ValidateBy,
-  ValidationArguments,
   ValidationOptions,
   IsInt,
 } from 'class-validator';
 import { differenceInSeconds } from 'date-fns';
 import { ApiProperty } from '@nestjs/swagger';
 
-const LOGIN_MESSAGE = process.env.LOGIN_MESSAGE || 'ROKETO-LOGIN';
-
-function StartsWith(
-  prefix: string,
-  validationOptions?: ValidationOptions,
-): PropertyDecorator {
-  return ValidateBy(
-    {
-      name: 'startsWith',
-      constraints: [prefix],
-      validator: {
-        validate: (value: any, args: ValidationArguments) =>
-          typeof value === 'string' && value.startsWith(args.constraints[0]),
-        defaultMessage: buildMessage(
-          (eachPrefix) =>
-            eachPrefix + '$property must start with "$constraint1"',
-          validationOptions,
-        ),
-      },
-    },
-    validationOptions,
-  );
-}
-
 const ACCEPTABLE_TIME_DEVIATION_IN_SECONDS = 60; // 1 minute
 
-function EndsWithCurrentTimestamp(
+function IsCurrentTimestamp(
   validationOptions?: ValidationOptions,
 ): PropertyDecorator {
   return ValidateBy(
     {
-      name: 'endsWithCurrentTimestamp',
+      name: 'isCurrentTimestamp',
       validator: {
         validate(value: any) {
-          if (typeof value !== 'string') {
-            return false;
-          }
-
-          const timestamp = Number(value.match(/\d+$/)?.[0] ?? 0);
+          const timestamp = Number(value);
 
           const diff = differenceInSeconds(timestamp, Date.now());
 
@@ -59,7 +30,7 @@ function EndsWithCurrentTimestamp(
         defaultMessage: buildMessage(
           (eachPrefix) =>
             eachPrefix +
-            `$property must end with timestamp deviating from current server time (${Date.now()}) for no more that 1 minute`,
+            `$property must not deviate from current server time (${Date.now()}) for more that 1 minute`,
           validationOptions,
         ),
       },
@@ -96,18 +67,12 @@ export class LoginDto {
   @IsNotEmpty()
   readonly accountId: string;
 
-  @ApiProperty({
-    description: `Login message starting with "${LOGIN_MESSAGE}" and ending with current timestamp`,
-    example: `${LOGIN_MESSAGE}-${Date.now()}`,
-  })
-  @IsString()
-  @IsNotEmpty()
-  @StartsWith(LOGIN_MESSAGE)
-  @EndsWithCurrentTimestamp()
-  readonly message: string;
+  @ApiProperty({ description: `Current timestamp.` })
+  @IsCurrentTimestamp()
+  readonly timestamp: number;
 
   @ApiProperty({
-    description: `Signature of login message signed with user's private key in form of an array of 64 integer numbers`,
+    description: `Signature of timestamp string signed with user's private key in form of an array of 64 integer numbers.`,
     example: Array.from({ length: 64 }).map((unused, index) => index),
   })
   @ArraySize(64)
