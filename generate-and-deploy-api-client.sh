27edff4f2d1f52902e68842ce2ce5705793905a3
Serge K Lebedev <hello@sergelebe.dev>
2022-04-08 01:57:22 +0600

    ROKETO-2 Add API client autodeploy (#4)

--- generate-and-deploy-api-client.sh
+++ generate-and-deploy-api-client.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
 
-# Generate swagger.json
+echo "⭐ Generate swagger.json"
 yarn
 GENERATE_SWAGGER_JSON=true yarn start
 
-# Clone client repo
+echo "⭐ Clone client repo"
 rm -rf roketo-api-client
 git clone --depth 1 git@github.com:roke-to/roketo-api-client.git
 
-# Regenerate client
+echo "⭐ Regenerate client"
 cd roketo-api-client
 CURRENT_VERSION=$(grep -Po "(?<=version\": \")\d+\.\d+\.\d+" package.json)
 npx openapi-generator-cli generate -i ../swagger.json -g typescript --additional-properties=npmName=@roketo/api-client,npmVersion=$CURRENT_VERSION,projectName=@roketo/api-client
 echo node_modules > .gitignore
 yarn
 
-# Commit changes, bump version and push
+echo "⭐ Commit changes, bump version and push"
+if [ "$CI" ]; then
+  git config user.name github-actions
+  git config user.email github-actions@github.com
+fi
 git add --all
 git commit -m "Regenerate API"
 yarn version --patch
 git push origin main --follow-tags
 
-# Clean
+echo "⭐ Clean up"
 cd ..
 rm swagger.json
 rm -rf roketo-api-client
