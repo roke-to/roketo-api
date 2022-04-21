3e1612077e1f05f27fbf1fccf317623d6c97dc4f
Serge K Lebedev <hello@sergelebe.dev>
2022-04-21 23:29:39 +0600

    ROKETO-34: Improve contract initialization

--- src/contract/contract.module.ts
+++ src/contract/contract.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { NearModule } from '../near/near.module';
-import { NearService } from '../near/near.service';
 import { ContractService } from './contract.service';
 
 @Module({
   imports: [NearModule],
-  providers: [
-    {
-      provide: ContractService,
-      useFactory: async (nearService) => {
-        const contractService = new ContractService(nearService);
-
-        await contractService.initContract();
-
-        return contractService;
-      },
-      inject: [NearService],
-    },
-  ],
+  providers: [ContractService],
   exports: [ContractService],
 })
 export class ContractModule {}
--- src/contract/contract.service.ts
+++ src/contract/contract.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Contract } from 'near-api-js';
 
 import { NearService } from '../near/near.service';
@@ -6,12 +6,12 @@ import { ROKETO_CONTRACT_NAME } from '../common/config';
 import type { RoketoContract } from '../common/contract.types';
 
 @Injectable()
-export class ContractService {
+export class ContractService implements OnModuleInit {
   constructor(private readonly nearService: NearService) {}
 
   contract: RoketoContract;
 
-  async initContract() {
+  async onModuleInit() {
     const account = await this.nearService.getAccount(ROKETO_CONTRACT_NAME);
 
     this.contract = new Contract(account, ROKETO_CONTRACT_NAME, {
