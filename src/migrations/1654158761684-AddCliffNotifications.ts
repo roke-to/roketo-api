69367619371ff1dcd4ddd68e00f91570efc4437c
Serge K Lebedev <hello@sergelebe.dev>
2022-06-07 00:22:41 +0600

    Format existing migrations

--- src/migrations/1654158761684-AddCliffNotifications.ts
+++ src/migrations/1654158761684-AddCliffNotifications.ts
@@ -1,20 +1,31 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddCliffNotifications1654158761684 implements MigrationInterface {
-    name = 'AddCliffNotifications1654158761684'
+  name = 'AddCliffNotifications1654158761684';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TYPE "public"."notification_type_enum" RENAME TO "notification_type_enum_old"`);
-        await queryRunner.query(`CREATE TYPE "public"."notification_type_enum" AS ENUM('StreamStarted', 'StreamPaused', 'StreamFinished', 'StreamIsDue', 'StreamContinued', 'StreamCliffPassed')`);
-        await queryRunner.query(`ALTER TABLE "notification" ALTER COLUMN "type" TYPE "public"."notification_type_enum" USING "type"::"text"::"public"."notification_type_enum"`);
-        await queryRunner.query(`DROP TYPE "public"."notification_type_enum_old"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TYPE "public"."notification_type_enum" RENAME TO "notification_type_enum_old"`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."notification_type_enum" AS ENUM('StreamStarted', 'StreamPaused', 'StreamFinished', 'StreamIsDue', 'StreamContinued', 'StreamCliffPassed')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "notification" ALTER COLUMN "type" TYPE "public"."notification_type_enum" USING "type"::"text"::"public"."notification_type_enum"`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."notification_type_enum_old"`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "public"."notification_type_enum_old" AS ENUM('StreamStarted', 'StreamPaused', 'StreamFinished', 'StreamIsDue', 'StreamContinued')`);
-        await queryRunner.query(`ALTER TABLE "notification" ALTER COLUMN "type" TYPE "public"."notification_type_enum_old" USING "type"::"text"::"public"."notification_type_enum_old"`);
-        await queryRunner.query(`DROP TYPE "public"."notification_type_enum"`);
-        await queryRunner.query(`ALTER TYPE "public"."notification_type_enum_old" RENAME TO "notification_type_enum"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."notification_type_enum_old" AS ENUM('StreamStarted', 'StreamPaused', 'StreamFinished', 'StreamIsDue', 'StreamContinued')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "notification" ALTER COLUMN "type" TYPE "public"."notification_type_enum_old" USING "type"::"text"::"public"."notification_type_enum_old"`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."notification_type_enum"`);
+    await queryRunner.query(
+      `ALTER TYPE "public"."notification_type_enum_old" RENAME TO "notification_type_enum"`,
+    );
+  }
 }
