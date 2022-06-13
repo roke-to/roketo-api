03ef4974cffdc5aaa94ab41a85ae9db497ae7d9a
Serge K Lebedev <hello@sergelebe.dev>
2022-06-13 23:55:03 +0600

    ROKETO-76: Add new fields and mark existing as optional in UpdateUserDto

--- src/users/update-user.dto.ts
+++ src/users/update-user.dto.ts
@@ -1,18 +1,26 @@
-import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, ValidateIf } from 'class-validator';
-import { Exclude } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsEmail,
+  IsOptional,
+  IsString,
+  ValidateIf,
+} from 'class-validator';
 
 export class UpdateUserDto {
   @ApiProperty({ description: 'The name of a user.' })
   @IsString()
-  readonly name: string;
+  @IsOptional()
+  readonly name?: string;
 
   @ApiProperty({ description: 'The email of a user.' })
   @ValidateIf((object, value) => Boolean(value))
   @IsEmail()
-  readonly email: string;
+  @IsOptional()
+  readonly email?: string;
 
-  @ApiHideProperty()
-  @Exclude()
-  readonly isEmailVerified: boolean;
+  @ApiProperty({ description: "User's permission for email notifications." })
+  @IsBoolean()
+  @IsOptional()
+  readonly allowNotifications?: boolean;
 }
