f08b2aab83736320aad86398688da781e796dd21
k-zhdanova <k.zhdanova88@gmail.com>
2023-01-03 19:09:20 +0400

    worker port

--- src/worker.ts
+++ src/worker.ts
@@ -11,7 +11,7 @@ declare module "jest-worker" {
 export async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(WorkerModule)
   
-  process.env.WORKER_HTTP_PORT = process.env.WORKER_HTTP_PORT ?? '4002';
+  process.env.WORKER_HTTP_PORT = process.env.WORKER_HTTP_PORT ?? '4001';
   await app.listen(process.env.WORKER_HTTP_PORT);
   console.debug(`Worker is running on ${await app.getUrl()}`);
 }
